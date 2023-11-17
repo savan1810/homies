70abb74cb418a40c8e14fc87ac8f62cce262c371
SavanDhola1810 <123728753+SavanDhola1810@users.noreply.github.com>
2023-11-17 13:43:27 +0530

    fifth commit

--- app/login/page.js
+++ app/login/page.js
@@ -22,21 +22,24 @@ const Page = () => {
             <p className='text-4xl mt-4 font-semibold text-black'>Login to continue</p>
           </div>
           <div className=' lg:w-3/5 border  bg-white shadow-xl  rounded-2xl mx-auto flex justify-center items-center  sm:my-10 my-16 '>
-            <div className=' z-10 px-4   mx-auto flex flex-col items-center justify-center sm:gap-10 gap-20  '>
+            <div className=' z-10 px-4  w-full mx-auto flex flex-col items-center justify-center sm:gap-10 gap-20  '>
               <div className='text-2xl   mb-4 text-center  tracking-wide'>
                 <p className='font-semibold'>HOMIES</p>
                 <p className="mt-4 text-xl">Let&lsquo;s Login</p>
               </div>
-              <form className="min-w-full mx-auto  ">
-                  
-                <TextField fullWidth label="Username" color="" id="fullWidth" />
-                <TextField fullWidth label="Password" id="fullWidth" className='mt-4'/>
+              <form className=" mx-auto w-full ">
+                 <div className='mb-2'>
+                    <TextField fullWidth label="Username" color="" id="fullWidth" />
+                 </div> 
+                 <div className='mt-2'>
+                    <TextField fullWidth label="Password" id="fullWidth" className=''/>
+                 </div>
                 
-                
-
-                <div className='py-4 text-center'>
+                <div className='w-full py-4 text-center'>
                                 <button className='w-full bg-gradient-to-r from-[#2980B9] to-[#6DD5FA]  my-2 text-xl p-4  outline-0 text-white rounded-md' >Login</button>
                             </div>
+                
+
                 {/* <button className='w-full bg-gradient-to-r from-[#2980B9] to-[#6DD5FA] text-xl p-4 my-4 outline-0 text-white rounded-md' >Login</button> */}
                 <div className='text-center mt-2 text-gray-400'>
                   <span>Don&lsquo;t have registered yet? </span>
